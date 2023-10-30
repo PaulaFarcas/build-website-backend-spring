f1492bc75026f1109e994e98bac2ae4e9d5a1a1f
PaulaFarcas <paulafarcas2002@yahoo.com>
2023-10-31 00:47:38 +0200

    show my orders for user

--- jwt-youtube-master/src/main/java/com/youtube/jwt/controller/OrderDetailController.java
+++ jwt-youtube-master/src/main/java/com/youtube/jwt/controller/OrderDetailController.java
@@ -1,11 +1,14 @@
 package com.youtube.jwt.controller;
 
+import com.youtube.jwt.entity.OrderDetail;
 import com.youtube.jwt.entity.OrderInput;
 import com.youtube.jwt.service.OrderDetailService;
 import com.youtube.jwt.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OrderDetailController {
@@ -18,5 +21,10 @@ public class OrderDetailController {
     public void placeOrder(@PathVariable(name = "isSingleProductCheckout")boolean isSingleProductCheckout,
                            @RequestBody OrderInput orderInput){
         orderDetailService.placeOrder(orderInput,isSingleProductCheckout);
+    }
+    @PreAuthorize("hasRole('User')")
+    @GetMapping("/getOrderDetails")
+    public List<OrderDetail> getOrderDetails(){
+        return orderDetailService.getOrderDetails();
     }
 }
--- jwt-youtube-master/src/main/java/com/youtube/jwt/service/OrderDetailService.java
+++ jwt-youtube-master/src/main/java/com/youtube/jwt/service/OrderDetailService.java
@@ -28,6 +28,13 @@ public class OrderDetailService {
     @Autowired
     private CartDao cartDao;
 
+
+    public List<OrderDetail> getOrderDetails(){
+        String currentUser = JwtRequestFilter.CURRENT_USER;
+        User user=userDao.findById(currentUser).get();
+        return orderDetailDao.findByUser(user);
+    }
+
     public void placeOrder(OrderInput orderInput,boolean isSingleProductCheckout){
         List<OrderProductQuantity> productQuantityList = orderInput.getOrderProductQuantityList();
 
